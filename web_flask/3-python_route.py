c0154f73d1c74347a114656fa42024c12737044c
Gilt457 <lepatioborel@gmail.com>
2024-04-19 03:46:38 -0700

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,42 +1,46 @@
 #!/usr/bin/python3
-"""Initialize a Flask web application.
+"""
+Set up a Flask web server.
 
-This application is configured to listen on all public IPs.
-Defined routes:
-    /: Renders the string 'Hello HBNB!' when accessed.
-    /hbnb: Renders the string 'HBNB' when accessed.
-    /c/<text>: Renders 'C ' followed by the user-provided <text>.
-    /python/(<text>): Renders 'Python ' followed by the user-provided <text>.
+This server is set to be accessible from any IP address.
+Available endpoints:
+    /: Displays 'Hello HBNB!' when visited.
+    /hbnb: Displays 'HBNB' when visited.
+    /c/<text>: Displays 'C ' followed by the custom <text> provided customer.
+    /python/(<text>): Displays 'Python ' followed by the custom <text>.
 """
 from flask import Flask
 
+# Create an instance of the Flask class.
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Render the greeting 'Hello HBNB!'."""
+    """Display the welcome message 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Render the string 'HBNB'."""
+    """Display the acronym 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Render 'C ' followed by the user-provided <text>."""
+    """Display 'C ' followed by the visitor's input <text>, replacing"""
     return f"C {text.replace('_', ' ')}"
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """Render 'Python ' followed by the user-provided <text>."""
+    """Display 'Python ' followed by the visitor's input <text> replacing"""
     return f"Python {text.replace('_', ' ')}"
 
 
+# Run the application if this file is executed as the main program.
 if __name__ == "__main__":
+    # Start the Flask application on all available IPs at port 5000.
     app.run(host="0.0.0.0", port=5000)
