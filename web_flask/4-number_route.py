bc47ad574232b52412a22727e6ccb1e81d9da7b3
Gilt457 <lepatioborel@gmail.com>
2024-04-19 03:57:43 -0700

    update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,49 +1,57 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""
+Initializes a Flask web server.
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+This server is configured to listen on all IP addresses (0.0.0.0).
+Defined routes:
+    /: Returns 'Hello HBNB!' when accessed.
+    /hbnb: Returns 'HBNB' when accessed.
+    /c/<text>: Returns 'C' plus the text provided in the URL.
+    /python/(<text>): Returns 'Python' plus the text provided.
+    /number/<n>: Validates if <n> is an integer and returns.
 """
 from flask import Flask
 
+# Create an instance of the Flask class.
 app = Flask(__name__)
 
 
+# Define the route for the root URL and return a greeting.
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """Returns a greeting as a string."""
     return "Hello HBNB!"
 
 
+# Define the route for '/hbnb' URL and return a simple string.
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Returns a string 'HBNB'."""
     return "HBNB"
 
 
+# Define the route for '/c/' URL and dynamically.
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>"""
+    """Returns 'C' concatenated with the URL parameter"""
     return f"C {text.replace('_', ' ')}"
 
 
+# Define the route for '/python/' URL with an optional.
 @app.route("/python", defaults={'text': "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
-    """Displays 'Python' followed by the value of <text>"""
+    """Returns 'Python' concatenated with the URL"""
     return f"Python {text.replace('_', ' ')}"
 
 
+# Define the route for '/number/' URL and validate.
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if n is an integer."""
+    """Checks if the parameter is an integer"""
     return f"{n} is a number"
 
 
+# Check if the script is executed directly and start.
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
