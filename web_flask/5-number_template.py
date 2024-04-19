7685a59c7c8a2d8b7f6ac9523c0514158790ce31
Gilt457 <lepatioborel@gmail.com>
2024-04-19 04:07:41 -0700

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,57 +1,66 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Initializes a Flask web server.
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
-    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+This server is configured to listen on all IP addresses (0.0.0.0).
+Defined routes:
+    /: Returns 'Hello HBNB!' when accessed.
+    /hbnb: Returns 'HBNB' when this route is visited.
+    /c/<text>: Returns 'C' plus the text provided in the URL.
+    /python/(<text>): Returns 'Python' plus the text provided
+    /number/<n>: Returns '<n> is a number' if <n> is an integer.
+    /number_template/<n>: Serves an HTML page if <n> is an integer.
 """
 
+# Flask library imported to create the application
 from flask import Flask, render_template
 
+# Flask application instance created
 app = Flask(__name__)
 
 
+# Route to respond with 'Hello HBNB!'
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """Responds with 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
+# Route to respond with 'HBNB'
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
+    """Responds with 'HBNB'"""
     return "HBNB"
 
 
+# Route to display 'C' and the subsequent text
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>"""
+    """Responds with 'C' and the text from the URL"""
     return f"C {text.replace('_', ' ')}"
 
 
+# Route to display 'Python' and the subsequent text
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>"""
+    """Responds with 'Python' and the text from the URL'"""
     return f"Python {text.replace('_', ' ')}"
 
 
+# Route to confirm if the provided value is a number
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if <n> is an integer."""
+    """Confirms that the provided value is a number"""
     return f"{n} is a number"
 
 
+# Route to display an HTML page if the provided value is a number
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Displays an HTML page only if <n> is an integer."""
+    """Serves an HTML page if the value is a number"""
     return render_template("5-number.html", n=n)
 
 
+# Main execution point to run the Flask application
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
