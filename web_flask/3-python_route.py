efd8106f6a78f8bbd5ce653930eb2c20ab28a416
Gilt457 <lepatioborel@gmail.com>
2024-04-19 03:27:17 -0700

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
-"""Initialize a Flask web application.
 
-This application is configured to listen on all public.
-Defined routes:
-    /: Renders the string 'Hello HBNB!' when accessed.
-    /hbnb: Renders the string 'HBNB' when accessed.
-    /c/<text>: Renders 'C ' followed by the user-provided <text>.
-    /python/(<text>): Renders 'Python ' followed by the user-provided.
-"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """Render the greeting 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """Render the string 'HBNB'."""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Render 'C ' followed by the user-provided <text>"""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>')
+def display_c(text):
+    # Replace underscores with spaces
+    formatted_text = text.replace('_', ' ')
+    return f"C {formatted_text}"
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Render 'Python ' followed by the user-provided <text>"""
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+@app.route('/python/<text>')
+def display_python(text="is cool"):
+    # Replace underscores with spaces
+    formatted_text = text.replace('_', ' ')
+    return f"Python {formatted_text}"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
